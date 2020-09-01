f9f1b0f145d38fa98b3998693a37750ccd6628c5
Paulo Guedes <ph.guedes99@outlook.com>
2020-09-01 18:53:29 -0300

    tela de busca (básica) preciso alterar muita coisa

---
+++ app/src/main/java/com/example/appuniversitario/BuscaActivity.java
@@ -0,0 +1,14 @@
+package com.example.appuniversitario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.busca);
+    }
+}
