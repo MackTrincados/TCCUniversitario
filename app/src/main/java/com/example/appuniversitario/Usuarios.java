742d0b1ff7e465bb39e011e8fa44fe2f7ac499bd
Paulo Guedes <ph.guedes99@outlook.com>
2020-10-12 00:05:56 -0300

    Set Imagem Perfil

--- app/src/main/java/com/example/appuniversitario/Usuarios.java
+++ app/src/main/java/com/example/appuniversitario/Usuarios.java
@@ -1,23 +1,27 @@
 package com.example.appuniversitario;
 
+import android.widget.ImageView;
+
 public class Usuarios {
     public String idUsuarios;
     public String nome;
     public String email;
     public String senha;
     public String sobrenome;
+    public ImageView fotodeperfil;
     public String sobremim;
     public String telefone;
     public String sexo;
     public int semestre;
 
     public  Usuarios(){}
-    public  Usuarios(String nome,String email,String sobrenome,String senha)
+    public  Usuarios(String nome,String email,String sobrenome,String senha, ImageView fotodeperfil)
     {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.email = email;
         this.senha = senha;
+        this.fotodeperfil = fotodeperfil;
     }
 
 
